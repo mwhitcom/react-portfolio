514de4ded262026199034c042146360b4e4acb6e
Matt Whitcomb <mwhitcom@gmail.com>
2017-11-30 14:06:33 -0800

    configured CSS-modules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import './Home.css';
 
 class Home extends Component {
   render() {
     return (
       <div>
-        Hello
+        <h1 styleName='title'>Hello</h1>
       </div>
     );
   }
