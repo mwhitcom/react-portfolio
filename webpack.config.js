d8e199d4119c6d323a48acab9b558102ba215633
Matt Whitcomb <mwhitcom@gmail.com>
2017-11-30 12:40:42 -0800

    initial files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
+
+module.exports = {
+  entry: APP_DIR + '/index.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({'process.env': {'NODE_ENV': JSON.stringify('production')}}),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  ],
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
