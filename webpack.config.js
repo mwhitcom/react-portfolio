391ab106d96fb25ab21713b3249afb5b4d024d79
mwhitcom <mwhitcom@gmail.com>
2018-04-11 11:34:30 -0700

    updated webpack for production build

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,10 @@ module.exports = {
   context,
   entry: ['babel-polyfill', './index.js'],
   plugins: [
-    new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin()
   ],
   module: {
     loaders: [
