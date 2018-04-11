2aad44bd049408c4ee2f9dbfe736099c8b0c4e4e
mwhitcom <mwhitcom@gmail.com>
2018-04-11 12:59:26 -0700

    reworked project section, began work on modals

--- src/components/Projects/ProjectGrid.js
+++ src/components/Projects/ProjectGrid.js
@@ -1,14 +1,66 @@
 import React, { Component } from 'react';
+import Modal from 'react-modal';
 import './ProjectGrid.css';
 
 import Data from '../../utils/Data.js';
 import SingleProject from './SingleProject';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.75)'
+  }
+};
+
 class ProjectGrid extends Component {
+  constructor() {
+    super();
+    this.state = {
+      modalIsOpen: false
+    };
+  }
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  }
+
+  afterOpenModal = () => {
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  }
+
   render() {
-    const projects = Data.projects.map(project => <SingleProject project={project} />);
+    const projects = Data.projects.map(project => <SingleProject project={project} openModal={this.openModal} />);
     return (
       <div styleName="grid-container">
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onAfterOpen={this.afterOpenModal}
+          onRequestClose={this.closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
+          <button onClick={this.closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
         {projects}
       </div>
     );
--- src/utils/Data.js
+++ src/utils/Data.js
@@ -41,29 +41,37 @@ const Data = {
   },
   projects: [
     {
-      name: 'infamousPR',
-      image: '/assets/infamous.jpg',
+      name: 'infamous',
+      code_name: 'infamousPR',
+      sub: 'Serverless React/Redux',
+      image_main: '/assets/infamous_main.png',
       link: 'https://infamouspr.com/',
       about: 'Built a new company website and CMS from scratch. UI/UX design, front-end, back-end, and 3rd party integrations. Utilizing React/Redux in front-end and express, Google Cloud Functions, and Firestore in back-end.',
       tech: ['React.js', 'Redux', 'Redux-Thunk', 'CSS Modules', 'Webpack', 'Firebase', 'Google Cloud Functions', 'Wire Framing', 'Architecture Planning']
     },
     {
       name: 'datavest',
-      image: '/assets/datavest.png',
+      code_name: 'datavest',
+      sub: 'Javascript',
+      image_main: '/assets/datavest_main.png',
       link: 'https://datavest.org/',
       about: 'Contract Front End Developer tasked with creating a basic informational website for financial app startup.',
       tech: ['HTML', 'CSS', 'jQuery', 'Parcel', 'Design Documents']
     },
     {
-      name: 'playlistRadio',
-      image: '/assets/playlistradio.jpg',
+      name: 'playlist radio',
+      code_name: 'playlistRadio',
+      sub: 'React/Redux',
+      image_main: '/assets/playlist_main.png',
       link: 'https://www.playlistrad.io/',
       about: 'Contract Full Stack Developer for Playlist Radio, an online playlist creator and aggregator tool for curators. Assisted in project management of dev team in India, built react components, coded Facebook Instant Articles integration, SEO',
       tech: ['React.js', 'Redux', 'RSS Feeds', 'SEO', 'Facebook Dev Tools', 'Project Management']
     },
     {
-      name: 'dataLA',
-      image: '/assets/datala.png',
+      name: 'data la',
+      code_name: 'dataLA',
+      sub: 'Javascript',
+      image_main: '/assets/datala.png',
       link: 'private',
       about: 'Full Stack Contract Developer work with DataLA, City of Los Angeles Web Development. Developed an ETL pipeline website with a team to improve efficiency of data querying for employees of the House + Community Investment Department.',
       tech: ['Node.js', 'Express', 'PostgreSQL', 'Data Streams', 'Sequelize', 'HandlebarsJS', 'Babel', 'es2017']
